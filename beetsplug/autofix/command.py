92e725ad67510bb389ab1a75abbf4fef48f8a8b0
Adam Jakab <jakabadambalazs@gmail.com>
2020-03-23 15:34:56 +0100

    genre control

--- beetsplug/autofix/command.py
+++ beetsplug/autofix/command.py
@@ -10,13 +10,16 @@ import time
 from optparse import OptionParser
 
 from beets import util
+from beets.dbcore.db import Results
 from beets.library import Library, Item, parse_query_parts
 from beets.ui import Subcommand, decargs
 from beets.util.confit import Subview
 
 from beetsplug.autofix import common
-from beetsplug import convert
-from beetsplug.convert import ConvertPlugin
+
+from beetsplug.zero import ZeroPlugin
+
+from beetsplug.lastgenre import LastGenrePlugin
 
 # The plugin
 __PLUGIN_NAME__ = u'autofix'
@@ -28,6 +31,8 @@ class AutofixCommand(Subcommand):
     lib: Library = None
     query = None
     parser: OptionParser = None
+
+    items: Results = None
 
     cfg_max_exec_time = None
 
@@ -83,45 +88,123 @@ class AutofixCommand(Subcommand):
     def handle_main_task(self):
         self.exec_time_start = int(time.time())
         try:
+            self.items = self._retrieve_library_items()
+            self._say("Total number of items in library: {}".format(len(self.items)))
             self.check_nonexistent_files()
             self.convert_non_mp3_files()
             self.convert_high_bitrate_files()
+            self.zero_unwanted_tags()
+            self.set_genre()
         except TimeoutError:
             self._say("Time is up! {} seconds have passed.".format(self.cfg_max_exec_time))
 
+    def set_genre(self):
+        items = []
+        plg = LastGenrePlugin()
+        for item in self.items:
+            item: Item
+            genre = item.get("genre")
+            if genre is None or genre == "":
+                items.append(item)
+
+        self._say("Checking missing genre for items({})...".format(len(items)))
+        for item in items:
+            self.check_timer()
+            item: Item
+            genre = item.get("genre")
+            if genre is None or genre == "":
+                genre, src = plg._get_genre(item)
+                if genre:
+                    self._say("Got Lastfm genre [genre: {}][source: {}] for item: {}"
+                              .format(genre, src, item.evaluate_template('$path')))
+                    item.genre = genre
+                    item.store()
+                    item.write()
+                    continue
+
+                # Use `genre_rosamerica` - better than nothing
+                genre_rosamerica = item.get("genre_rosamerica")
+                if genre_rosamerica in common.ROSAMERICA_GENRES.keys():
+                    genre = common.ROSAMERICA_GENRES[genre_rosamerica]
+                    self._say("Got Genre ROSAMERICA genre [genre: {}] for item: {}"
+                              .format(genre, item.evaluate_template('$path')))
+                    item.genre = genre
+                    item.store()
+                    item.write()
+                    continue
+
+                # if we are still here then there is no way of knowing the genre
+                self._say("Unable to figure out genre for item: {}").format(item.evaluate_template('$path'))
+
+    def zero_unwanted_tags(self):
+        items = []
+        plg = ZeroPlugin()
+        plg_cfg = common.get_plugin_config("zero")
+        if not plg_cfg or not plg_cfg["fields"].exists():
+            self._say("No Zero plugin field config found. Scanning entire library.")
+            items = self.items
+        else:
+            id_reg = []
+            for fld in plg_cfg["fields"].get():
+                for item in self.items:
+                    item: Item
+                    if item.id not in id_reg:
+                        if len(str(item.get(fld))) > 0:
+                            id_reg.append(item.id)
+                            items.append(item)
+
+        self._say("Checking non-zeroed items({})...".format(len(items)))
+        for item in items:
+            self.check_timer()
+            item: Item
+            self._say("Zeroing Item: {}".format(item.evaluate_template('$path')))
+            plg.process_item(item)
+
     def convert_high_bitrate_files(self):
-        items = self._retrieve_library_items()
-        self._say("Checking high bitrate items({})...".format(len(items)))
+        items = []
+        for item in self.items:
+            item: Item
+            bitrate = int(item.get("bitrate"))
+            if bitrate > 182000:
+                items.append(item)
+
+        self._say("Converting high bitrate items({})...".format(len(items)))
         for item in items:
             self.check_timer()
             item: Item
             bitrate = int(item.get("bitrate"))
-            if bitrate > 182000:
-                self._say("Converting high bitrate ({}) to lower: {}".format(bitrate, item.get("path")))
-                common.convert_item(item, self.temp_path)
+            self._say("Converting high bitrate ({}) to lower: {}".format(bitrate, item.get("path")))
+            common.convert_item(item, self.temp_path)
 
     def convert_non_mp3_files(self):
-        items = self._retrieve_library_items()
-        self._say("Checking non-mp3 items({})...".format(len(items)))
-
-        for item in items:
-            self.check_timer()
+        items = []
+        for item in self.items:
             item: Item
             fmt = item.get("format").lower()
             if fmt != "mp3":
-                self._say("Converting {} to mp3: {}".format(fmt, item.get("path")))
-                common.convert_item(item, self.temp_path)
+                items.append(item)
+
+        self._say("Converting non-mp3 items({})...".format(len(items)))
+        for item in items:
+            self.check_timer()
+            item: Item
+            self._say("Converting to mp3: {}".format(item.get("path")))
+            common.convert_item(item, self.temp_path)
 
     def check_nonexistent_files(self):
-        items = self._retrieve_library_items()
-        self._say("Checking nonexistent items({})...".format(len(items)))
-        for item in items:
+        self._say("Checking deleted items({})...".format(len(self.items)))
+        remove_count = 0
+        for item in self.items:
             self.check_timer()
             item: Item
             filepath = item.get("path")
             if not os.path.isfile(filepath):
-                self._say("Removing item for missing file: {}".format(filepath))
+                remove_count += 1
                 item.remove()
+
+        if remove_count > 0:
+            self._say("Removed missing items: {}".format(remove_count))
+            self.items = self._retrieve_library_items()
 
     def _retrieve_library_items(self, query=None, model_cls=Item):
         query = [] if query is None else query
--- beetsplug/autofix/common.py
+++ beetsplug/autofix/common.py
@@ -11,11 +11,23 @@ import subprocess
 import tempfile
 
 from beets import util
+from beets import config
 from beets.library import Item
 from beetsplug import convert
 from beetsplug.convert import ConvertPlugin
 
 __logger__ = logging.getLogger('beets.autofix')
+
+ROSAMERICA_GENRES = {
+    "cla": "classical",
+    "dan": "dance",
+    "hip": "hip-hop",
+    "jaz": "jazz",
+    "pop": "pop",
+    "rhy": "rhythm and blues",
+    "roc": "rock",
+    "spe": "speech"
+}
 
 
 def convert_item(item: Item, temp_path):
@@ -39,6 +51,13 @@ def convert_item(item: Item, temp_path):
     item.move()
     item.store()
 
+
+def get_plugin_config(plugin_name):
+    cfg = None
+    if config[plugin_name].exists():
+        cfg = config[plugin_name]
+
+    return cfg
 
 def say(msg, log_only=False):
     """Log and write to stdout
