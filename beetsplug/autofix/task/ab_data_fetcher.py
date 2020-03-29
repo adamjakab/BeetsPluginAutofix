a5129cf3007818a22e5afc8409d79185bdfdfc3d
Adam Jakab <jakabadambalazs@gmail.com>
2020-03-29 16:14:58 +0200

    task ab_data_fetcher - removed beets-xtractor dependency

--- beetsplug/autofix/task/ab_data_fetcher.py
+++ beetsplug/autofix/task/ab_data_fetcher.py
@@ -11,6 +11,70 @@ from beetsplug.autofix.task import Task
 
 from beetsplug.acousticbrainz import AcousticPlugin
 
+target_map = {
+    'average_loudness': {
+        'path': 'lowlevel.average_loudness',
+        'type': float,
+    },
+    'bpm': {
+        'path': 'rhythm.bpm',
+        'type': int,
+    },
+    'danceable': {
+        'path': 'highlevel.danceability.all.danceable',
+        'type': float,
+    },
+    'gender': {
+        'path': 'highlevel.gender.value',
+        'type': str,
+    },
+    'genre_rosamerica': {
+        'path': 'highlevel.genre_rosamerica.value',
+        'type': str,
+    },
+    'voice_instrumental': {
+        'path': 'highlevel.voice_instrumental.value',
+        'type': str,
+    },
+    'mood_acoustic': {
+        'path': 'highlevel.mood_acoustic.all.acoustic',
+        'type': float,
+    },
+    'mood_aggressive': {
+        'path': 'highlevel.mood_aggressive.all.aggressive',
+        'type': float,
+    },
+    'mood_electronic': {
+        'path': 'highlevel.mood_electronic.all.electronic',
+        'type': float,
+    },
+    'mood_happy': {
+        'path': 'highlevel.mood_happy.all.happy',
+        'type': float,
+    },
+    'mood_party': {
+        'path': 'highlevel.mood_party.all.party',
+        'type': float,
+    },
+    'mood_relaxed': {
+        'path': 'highlevel.mood_relaxed.all.relaxed',
+        'type': float,
+    },
+    'mood_sad': {
+        'path': 'highlevel.mood_sad.all.sad',
+        'type': float,
+    }
+
+    # 'chords_changes_rate': types.Float(6),
+    # 'chords_key': types.STRING,
+    # 'chords_number_rate': types.Float(6),
+    # 'chords_scale': types.STRING,
+    # 'initial_key': types.STRING,
+    # 'key_strength': types.Float(6),
+    # 'rhythm': types.Float(6),
+    # 'tonal': types.Float(6),
+}
+
 
 class ABDataFetcherTask(Task):
     plugin = None
@@ -19,9 +83,10 @@ class ABDataFetcherTask(Task):
 
     def __init__(self):
         super(ABDataFetcherTask, self).__init__()
-        assert common.is_plugin_enabled("acousticbrainz"), "The 'acousticbrainz' plugin is not enabled!"
+        assert common.is_plugin_enabled(
+            "acousticbrainz"), "The 'acousticbrainz' plugin is not enabled!"
         self.plugin = AcousticPlugin()
-        self.plugin_config = common.get_plugin_config("xtractor")
+        self.plugin_config = common.get_plugin_config("acousticbrainz")
 
     def setup(self, config, item):
         self.config = config
@@ -43,66 +108,56 @@ class ABDataFetcherTask(Task):
             return
 
         self._say("Got clean AB data: {}".format(audiodata), log_only=True)
-        self.has_new_data = True
         for attr in audiodata.keys():
-            if audiodata.get(attr):
+            # todo: add force option
+            if audiodata.get(attr) and not self.item.get(attr):
                 setattr(self.item, attr, audiodata.get(attr))
+                self.has_new_data = True
 
     def _extract_from_abdata(self, abdata):
-        """extracts data from the low level json file as mapped out in the `low_level_targets` configuration key
+        """extracts data from the low level json file as mapped out in the
+        `low_level_targets` configuration key
         """
         data = {}
+        for key in target_map.keys():
+            try:
+                val = self._extract_value_from_abdata(abdata,
+                                                      target_map[key])
+            except AttributeError:
+                val = None
 
-        target_maps = ["low_level_targets", "high_level_targets"]
-        for map_key in target_maps:
-            target_map = self.plugin_config[map_key]
-            for key in target_map.keys():
-                try:
-                    val = self._extract_value_from_abdata(abdata, target_map[key])
-                except AttributeError:
-                    val = None
-
-                data[key] = val
+            data[key] = val
 
         return data
 
     @staticmethod
     def _extract_value_from_abdata(abdata, target_map_item: Subview):
-        path: str = target_map_item["path"].as_str()
-        value_type = target_map_item["type"].as_str()
+        path: str = target_map_item["path"]
+        value_type = target_map_item["type"]
         path_parts = path.split(".")
+
         for part in path_parts:
             if not part in abdata:
-                raise AttributeError("No path '{}' found in abdata".format(path))
+                raise AttributeError(
+                    "No path '{}' found in abdata".format(path))
             abdata = abdata[part]
 
-        if value_type == "string":
-            value = str(abdata)
-        elif value_type == "float":
-            value = float(abdata)
-        elif value_type == "integer":
-            value = int(round(float(abdata)))
-        else:
-            value = abdata
-
-        return value
+        return value_type(abdata)
 
     def _item_needs_processing(self):
         answer = False
 
         if not self.item.mb_trackid:
-            return False
+            self._say("Item does not have `mb_trackid`. Cannot proceed.",
+                      log_only=True)
+            return answer
 
-        target_maps = ["low_level_targets", "high_level_targets"]
-        for map_key in target_maps:
-            if answer:
+        # Check attributes listed in the AB plugin
+        for fld in target_map.keys():
+            if not self.item.get(fld):
+                self._say("Missing Fld: {}".format(fld), log_only=True)
+                answer = True
                 break
-            target_map = self.plugin_config[map_key]
-            for fld in target_map:
-                if target_map[fld]["required"].exists() and target_map[fld]["required"].get(bool):
-                    if not self.item.get(fld):
-                        answer = True
-                        break
 
         return answer
 
